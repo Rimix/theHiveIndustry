b786c67480d4d9474be89589ff11d9d0e1b37c3f
Victor <rimix92@gmail.com>
2013-06-19 17:01:16 +0200

    premiere version

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=UTF-8 />
+    <title>Les bananes de Jeff</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="favicon/favicon.ico"/>
+    <meta name="viewport" content="width=device-width">
+
+    <!--[if lte IE 8]>
+        <link rel="stylesheet" href="css/ie8.css" type="text/css" media="screen,projection" />
+        <script src="js/script-html5.js"></script>
+    <![endif]--> 
+        <!--[if lte IE 7]>
+        <link rel="stylesheet" href="css/ie7.css" type="text/css" media="screen,projection" />
+    <![endif]--> 
+
+
+    <link rel="stylesheet" href="css/themes/base/jquery.ui.all.css">
+    <script type="text/javascript" src="js/jquery-1.9.1.js"></script>
+    <script src="js/ui/jquery.ui.core.js"></script>
+    <script src="js/ui/jquery.ui.widget.js"></script>
+    <script src="js/ui/jquery.ui.mouse.js"></script>
+    <script src="js/ui/jquery.ui.slider.js"></script>
+    
+
+
+    <link rel="stylesheet" href="css/style.css" type="text/css" />
+    <link rel="stylesheet" href="css/demos.css">
+
+
+
+
+<body>
+    <div id="wrapper">
+        <div class="slider">
+            <div class="scroll-pane ui-widget ui-widget-header ui-corner-all">
+                <ul class="scroll-content">
+                    <li class="scroll-content-item ui-widget-header">
+                        <!-- <img  src="img/photo1.jpg" > -->
+                        <div style="background:; display:block;">
+                            coucou
+                        </div>
+                    </li>
+                    <li class="scroll-content-item ui-widget-header">coucou</li>
+                    <li class="scroll-content-item ui-widget-header">3piopioi</li>
+                    <li class="scroll-content-item ui-widget-header">4</li>
+                    <li class="scroll-content-item ui-widget-header">5</li>
+                    <li class="scroll-content-item ui-widget-header">6</li>
+                    <li class="scroll-content-item ui-widget-header">7</li>
+                    <li class="scroll-content-item ui-widget-header">8</li>
+                    <li class="scroll-content-item ui-widget-header">9</li>
+                    <li class="scroll-content-item ui-widget-header">10</li>
+                    <li class="scroll-content-item ui-widget-header">11</li>
+                    <li class="scroll-content-item ui-widget-header">12</li>
+                    <li class="scroll-content-item ui-widget-header">13</li>
+                    <li class="scroll-content-item ui-widget-header">14</li>
+                    <li class="scroll-content-item ui-widget-header">15</li>
+                    <li class="scroll-content-item ui-widget-header">16</li>
+                    <li class="scroll-content-item ui-widget-header">17</li>
+                    <li class="scroll-content-item ui-widget-header">18</li>
+                    <li class="scroll-content-item ui-widget-header">19</li>
+                    <li class="scroll-content-item ui-widget-header">20</li>
+                </ul>
+            </div>
+            <div class="scroll-bar-wrap ui-widget-content ui-corner-bottom">
+                <div class="scroll-bar"></div>
+            </div>
+        </div>
+    </div>
+  
+<!-- JavaScript at the bottom for fast page loading -->
+
+
+<!-- Javascript file -->
+<script src="js/script.js"></script>
+<script>
+
+</script>
+</body>
+</html>
